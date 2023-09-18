c854ebdd84f501bcd36a99f843e2b381f0830d1e
Jaime Morgan Hitchcock <jaime@surrealdb.com>
2023-09-18 14:05:09 +0100

    Update 'Open Source' page

--- app/components/section/opensource/index.js
+++ app/components/section/opensource/index.js
@@ -5,10 +5,13 @@ export default class extends Component {
 
 	@cache get model() {
 		return [
+			{ name: 'SurrealML', link: 'https://github.com/surrealdb/surrealml', lang: 'Rust / Apache-2.0', text: 'A machine learning file format for working with and packaging PyTorch and SKLearn models in Python, using Rust', image: '/static/img/opensource/ml.png' },
+			{ name: 'SurrealKV', link: 'https://github.com/surrealdb/surrealkv', lang: 'Rust / Apache-2.0', text: 'A low-level, versioned, embedded, ACID-compliant, key-value database for Rust, with support for multiple concurrent readers and writers', image: '/static/img/opensource/kv.png' },
 			{ name: 'EchoDB', link: 'https://github.com/surrealdb/echodb', lang: 'Rust / Apache-2.0', text: 'An embedded, in-memory, immutable, copy-on-write database engine with serialisable, isolated transactions', image: '/static/img/opensource/echodb.png' },
-			{ name: 'YokuDB', link: 'https://github.com/surrealdb/yokudb', lang: 'Rust / Apache-2.0', text: 'A versioned, embedded, persistent, memory-mapped, ACID-compliant key-value database engine', image: '/static/img/opensource/yokudb.png' },
 			{ name: 'IndxDB', link: 'https://github.com/surrealdb/indxdb', lang: 'Rust / Apache-2.0', text: 'A key-value database engine abstraction layer for IndexedDB in the browser using WASM', image: '/static/img/opensource/indxdb.png' },
 			{ name: 'storekey', link: 'https://github.com/surrealdb/storekey', lang: 'Rust / Apache-2.0', text: 'A binary encoding serialization format for Serde, which ensures lexicographic sort ordering', image: '/static/img/opensource/storekey.png' },
+			{ name: 'Revision', link: 'https://github.com/surrealdb/revision', lang: 'Rust / Apache-2.0', text: 'A Rust library for revision-tolerant serialisation and deserialisation, with support for schema evolution over time', image: '/static/img/opensource/revision.png' },
+			{ name: 'Tart', link: 'https://github.com/surrealdb/tart', lang: 'Rust / Apache-2.0', text: 'An immutable, copy-on-write, snapshot-based, timed-adaptive radix trie data structure library  for Rust', image: '/static/img/opensource/tart.png' },
 			{ name: 'RixxDB', link: 'https://github.com/surrealdb/rixxdb', lang: 'Golang / Apache-2.0', text: 'A versioned, embedded, strongly-consistent, ACID-compliant key-value database engine', image: '/static/img/opensource/rixxdb.png' },
 			{ name: 'cork', link: 'https://github.com/surrealdb/cork', lang: 'Golang / Apache-2.0', text: 'A self-describing, binary data serialisation format for Golang', image: '/static/img/opensource/cork.png' },
 			{ name: 'bump', link: 'https://github.com/surrealdb/bump', lang: 'Golang / Apache-2.0', text: 'An efficient and performant data buffering library for Golang', image: '/static/img/opensource/bump.png' },
